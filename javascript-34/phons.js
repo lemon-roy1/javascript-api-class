54f17b25179e59d2c91aad00d61a7f46d1706a58
Lemon roy (Nila) <you@example.com>
2022-09-01 03:14:44 -0700

    api and javascript project file

--- javascript-34/phons.js
+++ javascript-34/phons.js
@@ -6,7 +6,7 @@ const loadPhons =async(searchText,dataLimit)=>{
 }
 displayPhon =( phons,dataLimit) =>{
     const phonsContainer =document.getElementById('phons-container');
-    phonsContainer.innerText=``;
+    phonsContainer.innerText='';
 
     // only phons 10ta display
     const showAll = document.getElementById('sholl-all');
@@ -39,7 +39,8 @@ displayPhon =( phons,dataLimit) =>{
             <div class="card-body">
                 <h5 class="card-title">${phone.phone_name}</h5>
                 <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                <button href="#" class="btn btn-primary">Show datails</button>
+                <button onclick="phonsDetails('${phone.slug}')" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#staticBackdrop">show detail</button>
+                
             </div>
         </div>
         `;
@@ -62,6 +63,16 @@ document.getElementById('btn-search').addEventListener('click',function (){
     perassSearch(10)
 })
 
+
+// enter input
+document.getElementById("search-field").addEventListener("keydown", function(e) {
+   console.log(e.key)
+    if (e.key === "Enter") {
+        perassSearch(10)
+    }
+});
+
+
 const togglespiner = isloader =>{
     const loaders =document.getElementById('loader')
     if(isloader){
@@ -75,4 +86,26 @@ const togglespiner = isloader =>{
 document.getElementById('btn-show-all').addEventListener('click',function(){
     perassSearch()
 })
-// loadPhons()
+
+const phonsDetails =async id =>{
+    const url =`https://openapi.programming-hero.com/api/phone/${id}`
+     const res =await fetch(url);
+     const data =await res.json()
+     phonDateilsModal(data.data);
+}
+const phonDateilsModal = phone =>{
+ const totalModaltital = document.getElementById('staticBackdropLabel');
+ totalModaltital.innerText=phone.name
+ 
+ const phonesDetail =document.getElementById('phone-details')
+ phonesDetail.innerHTML =`
+ <p>Phone release Date: ${phone.releaseDate ? phone.releaseDate:'Not releas-Date found'}</p>
+ <p>memory : ${phone.mainFeatures.memory}</p>
+ <p>ChipStet : ${phone.mainFeatures.chipSet}</p>
+ <p>Storage : ${phone.mainFeatures.storage}</p>
+ <p>DisPlay - Size: ${phone.mainFeatures.displaySize}</p>
+ `
+}
+
+
+// loadPhons('oppo')
