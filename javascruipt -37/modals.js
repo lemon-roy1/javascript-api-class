bfe210fd31f06d40458c96b4f41424fbbbd11673
Lemon roy (Nila) <you@example.com>
2022-09-07 14:00:28 -0700

    class 37 datail math

---
+++ javascruipt -37/modals.js
@@ -0,0 +1,24 @@
+// first btn
+const alartdatail=()=>{
+    alert('kamon aso dosto')
+ }
+// secunt btn
+ const asksconfrim=()=>{
+    const datail= confirm('tumi ki job korta jaba');
+    if (!!datail){
+        alert('tk da 500 tk')
+    }
+    else{
+        alert('dura gaya mor na gala');
+
+    }
+ }
+//  their btn
+const getInfodata=()=>{
+const name= prompt ('i am fine')
+console.log(name);
+if(!!name){
+    console.log(' welcome to :', name)
+}
+    
+}
